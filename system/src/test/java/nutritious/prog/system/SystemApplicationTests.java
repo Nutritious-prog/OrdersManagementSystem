3cffbe27b01ac159b7c4a2701da4f1c5c97f52be
nutristious-prog <artur.wlodarczyk12@gmail.com>
2022-06-30 09:59:41 +0200

    Project setup

---
+++ system/src/test/java/nutritious/prog/system/SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package nutritious.prog.system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
